186e32ad55468ba619cdf6f5c7deb2f14cf042c0
Daniil <dan7326@yandex.ru>
2020-05-16 13:48:39 +0300

    add edits

--- src/main/java/interfaces/Man.java
+++
@@ -1,9 +0,0 @@
-package interfaces;
-
-public class Man implements Walkable,Talkable,Eatable {
-
-    @Override
-    public void walk() {
-        System.out.println("Я гуляю на двух ногах");
-    }
-}
