e854fd6fb101987d925eae11b13e11b5ae7878c0
Daniil <dan7326@yandex.ru>
2020-05-20 12:29:15 +0300

    deleted string

--- src/main/java/interfaces/hw2/PersonComparator.java
+++ src/main/java/interfaces/hw2/PersonComparator.java
@@ -3,9 +3,6 @@ package interfaces.hw2;
 import java.util.Comparator;
 public class PersonComparator {
 
-//    Comparator<Person> = new Comparator<Person>
-
-
     public int compare(Person p1,Person p2){
         return p1.compareTo(p2);
     }
