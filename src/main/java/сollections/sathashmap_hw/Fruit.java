150fff413a336a8060256fc8c83edbdb6c032401
Daniil <dan7326@yandex.ru>
2020-06-15 22:00:27 +0300

    add home work with Map

---
+++ src/main/java/сollections/sathashmap_hw/Fruit.java
@@ -0,0 +1,19 @@
+package сollections.sathashmap_hw;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Fruit {
+    private final String name;
+    private double weight;
+
+    @Override
+    public String toString() {
+        return "Fruit{" +
+                "name='" + name + '\'' +
+                ", weight=" + weight +
+                '}';
+    }
+}
