f21c85fe73a17a11c98024e138353729d73e7e77
Daniil <dan7326@yandex.ru>
2020-06-18 11:00:30 +0300

    add ExceptionHW edits

--- src/main/java/exceptions/hw/ExceptionHW.java
+++ src/main/java/exceptions/hw/ExceptionHW.java
@@ -1,25 +1,36 @@
 package exceptions.hw;
 
+import exceptions.Child;
+import exceptions.MyException;
+
 public class ExceptionHW  {
     public static void main(String[] args) {
 
 //        throw new NullPointerException("Null");
 
-        try {
-            throw new IndexOutOfBoundsException("Null");
-        } catch (IndexOutOfBoundsException e) {
-            System.out.println("index");
+//        try {
+//            throw new IndexOutOfBoundsException("Null");
+//        } catch (IndexOutOfBoundsException e) {
+//            System.out.println("index");
+//
+//        } finally {
+//            System.out.println("first finally");
+//        }
+//
+//        try {
+//            exception();
+//        } catch (Exception e) {
+//            System.out.println("Catch");
+//        }finally {
+//            System.out.println("second finally");
+//        }
 
-        } finally {
-            System.out.println("first finally");
-        }
-
         try {
-            exception();
-        } catch (Exception e) {
-            System.out.println("Catch");
+            Child tema = new Child("Tema",111);
+        } catch (MyException e) {
+            e.printStackTrace();
         }finally {
-            System.out.println("second finally");
+            System.out.println("Third finally");
         }
     }
 
--- src/main/java/exceptions/hw/Person.java
+++ src/main/java/exceptions/hw/Person.java
@@ -3,9 +3,16 @@ package exceptions.hw;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-@AllArgsConstructor
+//@AllArgsConstructor
 @Getter
 public class Person {
     private final String name;
     private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        if (age < 0 || age > 100) {
+            throw new MyExceptionHW("Введите корректный возраст");
+        }
+    }
 }
